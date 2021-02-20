20d102daae6dfa6ce3a645a5f1988d4777fc0921
abnerbessi <67401283+abnerbessi@users.noreply.github.com>
2021-02-20 00:06:48 -0300

    Atualização do Readme

--- model/AdaptadorFile.py
+++ model/AdaptadorFile.py
@@ -1,2 +1,25 @@
+import xml.dom.minidom;
+
 class AdaptadorFile:
     def __init__(self):
+        self.path = "";
+
+
+
+    def setFileDataSource(self,datasource):
+        #recuperando arquivo de conexões
+        connRepo = xml.dom.minidom.parse("./Arquivos/Adaptadores/conn.xml");
+        #recuperando tag oracle do arquivo
+        Olement = connRepo.getElementsByTagName("file");
+        # fazendo loop na tag oracle
+        for l_Olement in Olement:
+            # recuperando tag filha datasource
+            l_dataSourcet = l_Olement.getElementsByTagName("datasource");
+            # fazendo loop na tag dataSource
+            for element in l_dataSourcet:
+                if element.getAttribute('name') == datasource:
+                    self.path = element.getAttribute('path');
+
+
+    def getPath(self):
+        return self.path;
